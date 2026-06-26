db1dc28c891060c58da60a99425ff8906adba600
philippearnault32 <philippe.arnault@outlook.com>
2026-06-26 12:26:48 +0200

    Refactor demarrer.bat for improved output and flow

--- demarrer.bat
+++ demarrer.bat
@@ -2,35 +2,29 @@
 setlocal enabledelayedexpansion
 title IDE Collaboratif Auto-Pilote
 
-echo [1/3] Demarrage du serveur Node.js...
-start /b node backend.js
+echo [1/2] Demarrage du serveur Node.js...
+:: On lance le backend dans une fenetre separee qui se fermera a la fin
+start "Serveur Backend" /min node backend.js
 
-echo [2/3] Generation du tunnel internet en arriere-plan...
-:: On lance localtunnel et on redirige sa sortie vers un fichier texte temporaire
-start /b npx lt --port 8080 > tunnel.txt
+echo [2/2] Generation du tunnel internet...
+echo --------------------------------------------------
+echo Connexion aux serveurs Localtunnel en cours...
+echo Une fenetre de navigateur va s'ouvrir automatiquement.
+echo --------------------------------------------------
 
-echo Attente de la generation du lien...
-:wait
-timeout /t 1 /nobreak >nul
-if not exist tunnel.txt goto wait
-
-:: On cherche la ligne contenant "url:" dans le fichier texte
-for /f "tokens=2 delims= " %%a in ('findstr "url:" tunnel.txt') do (
+:: On lance localtunnel et on recupere directement sa sortie sans passer par un fichier texte
+for /f "tokens=2 delims= " %%a in ('npx lt --port 8080') do (
     set "URL=%%a"
+    goto launch
 )
 
-:: Si l'URL n'est pas encore ecrite, on patiente encore
-if "!URL!"=="" goto wait
-
-:: Nettoyage de l'URL pour ne garder que le nom de domaine
+:launch
+:: Nettoyage de l'URL pour le parametre
 set "CLEAN_URL=!URL:https://=!"
 set "CLEAN_URL=!CLEAN_URL:http://=!"
 
-echo [3/3] Lien genere : !URL!
-echo Ouverture automatique de l'IDE...
+echo Lien genere : !URL!
+echo Ouverture de l'IDE...
 
-:: On ouvre le index.html en lui injectant directement l'URL du tunnel !
+:: Ouverture automatique du navigateur
 start index.html?tunnel=!CLEAN_URL!
-
-:: Nettoyage du fichier temporaire
-del tunnel.txt
